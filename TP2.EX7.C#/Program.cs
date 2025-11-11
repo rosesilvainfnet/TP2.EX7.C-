1768a6aa902103cc52b5db1a3938ebe468f725c6
rosesilvainfnet <roseane.silva@al.infnet.edu.br>
2025-11-10 22:28:00 -0300

    Adicionar arquivos de projeto.

---
+++ TP2.EX7.C#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class VerificadorParImpar
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("--- Verificador de Paridade ---");
+
+        Console.Write("Digite um número inteiro: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int numero))
+        {
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("\n=============================================");
+                Console.WriteLine($"O número {numero} é PAR.");
+                Console.WriteLine("=============================================");
+            }
+            else
+            {
+                Console.WriteLine("\n=============================================");
+                Console.WriteLine($"O número {numero} é ÍMPAR.");
+                Console.WriteLine("=============================================");
+            }
+        }
+        else
+        {
+            Console.WriteLine("\nErro: Entrada inválida. Por favor, digite um número inteiro.");
+        }
+    }
+}
